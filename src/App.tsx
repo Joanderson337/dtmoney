a0234e8c7ed53d35b0ee770431ac9999357b96b7
Joanderson SIlva <jhonny_040996@hotmail.com>
2022-03-26 19:14:18 -0300

    header dtmoney

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { GlabalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
-      
-      <h1>Hello Word</h1>
-   
-    </div>
+    <>
+      <Header/>
+      <Dashboard/>
+      <GlabalStyle />
+    </>
   );
 }
-
-
