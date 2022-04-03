32758bc877143e68fffa9f243e282e10336fd4ab
Joanderson SIlva <jhonny_040996@hotmail.com>
2022-04-03 15:33:03 -0300

    tabela

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
-import { createServer} from 'miragejs'
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+import { createServer, Model } from "miragejs";
+
 
 createServer({
-  routes(){
-    this.namespace = 'api'
 
-    this.get('/transactions', () =>{
-      return[
+  models: {
+    transactions: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
         {
           id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposito',
-          category: 'ifood',
-          createdAt: new Date()
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-02-14 11:00:00'),
         }
-      ]
+      ],
     })
-  }
-})
+  },
 
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return this.schema.all('transaction')
+    });
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data)
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-
